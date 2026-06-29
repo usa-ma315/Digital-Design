1b93cd4de6ff774f74e135cf47b268ca96aa093a
Rana Usama Naeem <usamanaeem803@gmail.com>
2026-06-29 17:31:49 +0500

    Add Or Gate

---
+++ Basics/Logic_Gates/testbench/or_tb.v
@@ -0,0 +1,26 @@
+module or_tb;
+    reg a;
+    reg b;
+    wire y;
+    
+    or_gate uut(
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        
+        $dumpfile("or_tb.vcd");
+        $dumpvars(0, or_tb);
+        a =0;b=0;
+        #10 
+        a=0;b=1;
+        #10 
+        a=1;b=0;
+        #10 
+        a=1;b=1;
+        #10 
+        $finish;
+    end
+endmodule   
